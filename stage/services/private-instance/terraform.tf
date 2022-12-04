26aa0527b26af938f079734d901f084ba73cc76d
devfloors <csh47790@gmail.com>
2022-12-04 21:54:21 +0900

    add private instance

---
+++ stage/services/private-instance/terraform.tf
@@ -0,0 +1,29 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "devfloors"
+
+    workspaces {
+      name = "terraform-k8s-boilerplate-private-ec2"
+    }
+  }
+}
+
+output "context" {
+  value =  yamldecode(file(var.config_file)).context
+}
+
+output "config" {
+  value = yamldecode(templatefile(var.config_file, local.context))
+}
+
+locals {
+  context = yamldecode(file(var.config_file)).context
+  config  = yamldecode(templatefile(var.config_file, local.context))
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+  shared_credentials_file = "~/.aws/credentials"
+}
+
