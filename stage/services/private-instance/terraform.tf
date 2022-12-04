47f4b82ad7136c7ec5d0bb98bc85cd0a6052d719
devfloors <csh47790@gmail.com>
2022-12-04 22:46:26 +0900

    fix remote state, data to local

--- stage/services/private-instance/terraform.tf
+++ stage/services/private-instance/terraform.tf
@@ -22,6 +22,16 @@ locals {
   config  = yamldecode(templatefile(var.config_file, local.context))
 }
 
+locals {
+  remote_states = {
+    "network" = data.tfe_outputs.this["network"].values
+  }
+
+  vpc_id = local.remote_states["network"].vpc_id
+  public_subnets = local.remote_states["network"].public_subnets
+  private_subnets = local.remote_states["network"].private_subnets
+}
+
 provider "aws" {
   region = "ap-northeast-2"
   shared_credentials_file = "~/.aws/credentials"
